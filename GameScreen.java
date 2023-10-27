6ef5982f0f4af599169d0e3f7dadb4c96ffd4b2d
Nell <eemandersson@outlook.com>
2023-10-27 14:54:58 +0200

    logik för att knappen nytt spel ska ge ett nytt spelbräde

--- GameScreen.java
+++ GameScreen.java
@@ -2,52 +2,50 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class GameScreen extends JFrame {
-    // Konstanta variabler för storleken på spelbrädet
     private static final int GRID_SIZE = 4;
     private static final int NUM_TILES = GRID_SIZE * GRID_SIZE;
+    private final JPanel tileContainer;
 
-    // Konstruktor för klassen "GameScreen"
     public GameScreen() {
-
-        setTitle("Puzzle Game");
+        setTitle("Tile Game");
         setSize(500, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Skapar en layout för fönstret
         setLayout(new BorderLayout());
 
-        // Skapar en ruta för spelbrädet med ett grid layout
-        JPanel puzzlePanel = new JPanel(new GridLayout(GRID_SIZE, GRID_SIZE));
-        createGrid(puzzlePanel); // Skapa spelbrädet
-        add(puzzlePanel, BorderLayout.CENTER); // Lägg till spelbrädet i mitten av fönstret
+       tileContainer = new JPanel(new GridLayout(GRID_SIZE, GRID_SIZE));
+        createGrid();  // Initialiserar spelbrädet vid start
+        add(tileContainer, BorderLayout.CENTER);
 
-        // Skapar en knapp med texten "Nytt spel"
         JButton newGameButton = new JButton("Nytt spel");
-        add(newGameButton, BorderLayout.SOUTH); // Lägger till knappen längst ner i fönstret
+        newGameButton.addActionListener(e -> newGame());
+        add(newGameButton, BorderLayout.SOUTH);
     }
 
-    // Metod för att skapa spelbrädet
-    private void createGrid(JPanel panel) {
-        // Skapa en lista med nummer för brickorna (0 till 15)
-        ArrayList<Integer> tileNumberList = new ArrayList<>();
+    private void createGrid() {
+        tileContainer.removeAll();
+        List<Integer> tileNumbers = new ArrayList<>();
         for (int i = 0; i < NUM_TILES; i++) {
-            tileNumberList.add(i);
+            tileNumbers.add(i);
         }
 
-        // Blanda ordningen på brickorna slumpmässigt
-        Collections.shuffle(tileNumberList);
+        Collections.shuffle(tileNumbers);
 
-        // Skapa och lägg till knappar för varje bricka i panelen
-        for (int i = 0; i < NUM_TILES; i++) {
-            int number = tileNumberList.get(i);
-            JButton button = createTile(number);
-            panel.add(button);
+        for (Integer number : tileNumbers) {
+            tileContainer.add(createTile(number));
         }
+
+        tileContainer.revalidate();
+        tileContainer.repaint();
     }
 
-    // Metod för att skapa en bricka (knapp) med ett visst nummer
+
+    private void newGame() {
+        createGrid();  // Initialiserar spelbrädet igen för nytt spel
+    }
+
     private JButton createTile(int number) {
         String buttonText = "";
         if (number != 0) {
