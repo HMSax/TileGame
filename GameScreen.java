176fa54b6d063eb0853f0d40db0d355c0cd2e980
Nell <eemandersson@outlook.com>
2023-10-27 14:56:30 +0200

    kommentarer till koden

--- GameScreen.java
+++ GameScreen.java
@@ -5,8 +5,12 @@ import java.util.Collections;
 import java.util.List;
 
 public class GameScreen extends JFrame {
+
+    // Konstanta variabler för storleken på spelbrädet
     private static final int GRID_SIZE = 4;
     private static final int NUM_TILES = GRID_SIZE * GRID_SIZE;
+
+    // "Container" som håller alla spelplattor
     private final JPanel tileContainer;
 
     public GameScreen() {
@@ -19,28 +23,34 @@ public class GameScreen extends JFrame {
         createGrid();  // Initialiserar spelbrädet vid start
         add(tileContainer, BorderLayout.CENTER);
 
+        // Knapp för nytt spel
         JButton newGameButton = new JButton("Nytt spel");
-        newGameButton.addActionListener(e -> newGame());
+        newGameButton.addActionListener(e -> newGame()); //Händelsehanterare för knappen
         add(newGameButton, BorderLayout.SOUTH);
     }
 
     private void createGrid() {
+        // Tar bort tidigare brickor från containern
         tileContainer.removeAll();
+
+        // Skapa en lista med nummer för varje bricka
         List<Integer> tileNumbers = new ArrayList<>();
         for (int i = 0; i < NUM_TILES; i++) {
             tileNumbers.add(i);
         }
 
+        // Blandar siffrorna
         Collections.shuffle(tileNumbers);
 
+        // Lägger till brickorna till containern
         for (Integer number : tileNumbers) {
             tileContainer.add(createTile(number));
         }
 
+        // Uppdaterar containern för att visa nya brickor
         tileContainer.revalidate();
         tileContainer.repaint();
     }
-
 
     private void newGame() {
         createGrid();  // Initialiserar spelbrädet igen för nytt spel
